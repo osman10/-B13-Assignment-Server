326e035e162ae3fc2a53d478ac3e1ef2177dd0e5
Osman Goni <osman.mirpur55@gmail.com>
2026-06-29 07:08:06 +0600

    Add JWT successfully

--- server.js
+++ server.js
@@ -4,12 +4,34 @@ const express = require("express");
 
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
+
+// JWT token verify
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req?.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JWKS);
+        // console.log(payload);
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: "Forbidden" });
+    }
+};
 
 
 
@@ -57,8 +79,9 @@ app.get("/tutors/:id", async (req, res) => {
   }
 });
 
+
 // Add tutor
-app.post("/addtutor", async (req, res) => {
+app.post("/addtutor", verifyToken, async (req, res) => {
   try {
     await getClient();
     const tutorsCollection = client.db("Tutorfinder").collection("Tutors");
